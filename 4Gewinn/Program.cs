a9a37f59e84a5052ad75826e016580c278d3a2ad
User <User@K02-2521-P017>
2024-02-06 14:18:45 +0100

    Projektdateien hinzufügen.

---
+++ 4Gewinn/Program.cs
@@ -0,0 +1,55 @@
+﻿int[,] arr = new int[6, 7];
+
+Console.Write("Wie heißt du hund : ");
+string P1 = Console.ReadLine();
+
+Console.Write("und du Arschloch : ");
+string P2 = Console.ReadLine();
+
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = 0;
+    }
+}
+
+void printArr(int[,] arr)
+{
+    Console.WriteLine("--1---2---3---4---5---6---7--");
+    Console.WriteLine("_____________________________");
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+            Console.Write("|");
+
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($" {arr[i, j]} ");
+                Console.Write("|");
+            }
+            Console.WriteLine("");
+        }
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine("--1---2---3---4---5---6---7--");
+}
+
+
+bool runing = true;
+while(runing)
+{
+    printArr(arr);
+    Console.Write($"{P1} Wohin : ");
+    int P1input = Convert.ToInt32(Console.ReadLine());
+
+    arr[5, P1input - 1] = 8;
+
+    printArr(arr);
+    Console.Write($"{P2} Wohin : ");
+    int P2input = Convert.ToInt32(Console.ReadLine());
+
+    arr[5, P2input - 1] = 8;
+}
+
+arr[2,1] = 4;
---
+++ fgfdsg/Program.cs
@@ -0,0 +1,90 @@
+﻿Console.Write("wie viel Zeilen :");
+int zeile = Convert.ToInt32(Console.ReadLine());
+Console.Write("wie viel Spalten :");
+int spalte = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[zeile, spalte];
+
+Random rand = new Random();
+
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = rand.Next(0, 9);
+    }
+}
+
+
+
+void PrintArraySimple(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+    Console.WriteLine("------------------");
+}
+
+
+void PrintArrayColorful(int[,] arr)
+{
+    ConsoleColor defaultColor = Console.ForegroundColor;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            switch (arr[i, j])
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    break;
+                case 5:
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    break;
+                case 6:
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    break;
+                case 7:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+                case 8:
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    break;
+                case 9:
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+
+            //Console.Write(arr[i,j] != 0 ? arr[i,j] : " ");
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine("");
+    }
+
+
+    Console.ForegroundColor = defaultColor;
+    Console.WriteLine("------------------");
+}
+
+PrintArraySimple(arr);
+//PrintArrayColorful(arr);
